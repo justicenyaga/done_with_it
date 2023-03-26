71e1fc2346f1f17cbbf3833292a46ea1c41d63cc
Justice Nyaga <justiceevansmwema@gmail.com>
2023-03-26 16:52:50 +0300

    flexDirection

--- App.js
+++ App.js
@@ -3,10 +3,28 @@ import { StyleSheet, StatusBar, Platform, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ backgroundColor: "#fff", flex: 1 }}>
-      <View style={{ backgroundColor: "dodgerblue", flex: 3 }} />
-      <View style={{ backgroundColor: "gold", flex: 1 }} />
-      <View style={{ backgroundColor: "tomato", flex: 1 }} />
+    <View style={{ backgroundColor: "#fff", flex: 1, flexDirection: "row" }}>
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: 100,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "gold",
+          width: 100,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "tomato",
+          width: 100,
+          height: 100,
+        }}
+      />
     </View>
   );
 }
