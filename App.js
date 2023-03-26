bebb0582cdea433191b88d8ed1ea6721c016c668
Justice Nyaga <justiceevansmwema@gmail.com>
2023-03-26 17:11:24 +0300

    flexWrap and alignContent

--- App.js
+++ App.js
@@ -10,14 +10,15 @@ export default function App() {
         flexDirection: "row", // horizontal
         justifyContent: "center", // main axis
         alignItems: "center", // secondary axis
+        alignContent: "center",
+        flexWrap: "wrap",
       }}
     >
       <View
         style={{
           backgroundColor: "dodgerblue",
           width: 100,
-          height: 200,
-          alignSelf: "flex-start",
+          height: 300,
         }}
       />
       <View
@@ -31,7 +32,21 @@ export default function App() {
         style={{
           backgroundColor: "tomato",
           width: 100,
-          height: 50,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "grey",
+          width: 100,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "greenyellow",
+          width: 100,
+          height: 100,
         }}
       />
     </View>
