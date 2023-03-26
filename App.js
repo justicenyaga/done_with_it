417a10837a8ae50b9dbe05376f8f2350906c9ab0
Justice Nyaga <justiceevansmwema@gmail.com>
2023-03-26 15:03:39 +0300

    Alert - Alert

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet } from "react-native";
+import { Button, StyleSheet, Alert } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
@@ -8,7 +8,12 @@ export default function App() {
       <Button
         color="orange"
         title="Click Me"
-        onPress={() => alert("Hello World")}
+        onPress={() =>
+          Alert.alert("My Title", "My Message", [
+            { text: "Yes", onPress: () => console.log("Yes") },
+            { text: "No", onPress: () => console.log("No") },
+          ])
+        }
       />
     </SafeAreaView>
   );
