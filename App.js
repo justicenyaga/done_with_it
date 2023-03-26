ece0c9f92b5213a11fcd16c60aef58c0939fa073
Justice Nyaga <justiceevansmwema@gmail.com>
2023-03-26 14:26:52 +0300

    Texts

--- App.js
+++ App.js
@@ -3,9 +3,16 @@ import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
+  const handlePress = () => {
+    console.log("Text pressed");
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello React Native!</Text>
+      <Text numberOfLines={1} onPress={handlePress}>
+        Hello React Native!. Let's make this a very long long line. I wanna make
+        this a really really long line!.
+      </Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
