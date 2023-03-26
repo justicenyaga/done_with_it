44aa2161c346a9ee313405a86bad0c0765daaaa1
Justice Nyaga <justiceevansmwema@gmail.com>
2023-03-26 19:42:26 +0300

    add Welcome Screen

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return <View></View>;
+  return <WelcomeScreen />;
 }
