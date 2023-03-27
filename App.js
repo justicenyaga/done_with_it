ace60b32712abd9cddca5d4dbbb59c496de71a2d
Justice Nyaga <justiceevansmwema@gmail.com>
2023-03-27 05:22:41 +0300

    borders

--- App.js
+++ App.js
@@ -1,7 +1,25 @@
 import React from "react";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
+import { View } from "react-native";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: 100,
+          height: 100,
+          borderWidth: 10,
+          borderColor: "royalblue",
+          borderRadius: 50, // if the border radius is at least half of the width, the result will be a perfect circle
+        }}
+      ></View>
+    </View>
+  );
 }
