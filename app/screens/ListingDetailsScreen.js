d42fb9737b2bdeff5346acb885d924d06cb44ea1
Justice Nyaga <justiceevansmwema@gmail.com>
2023-03-27 10:37:48 +0300

    build ListItem component

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Image, StyleSheet, View } from "react-native";
 
 import AppText from "../components/AppText";
+import ListItem from "../components/ListItem";
 import colors from "../config/colors";
 
 function ListingDetailsScreen(props) {
@@ -11,6 +12,14 @@ function ListingDetailsScreen(props) {
       <View style={styles.detailsContainer}>
         <AppText style={styles.title}>Red jacket for sale</AppText>
         <AppText style={styles.price}>$100</AppText>
+
+        <View style={styles.userContainer}>
+          <ListItem
+            image={require("../assets/justice.jpg")}
+            title="Justice Nyaga"
+            subTitle="5 Listings"
+          />
+        </View>
       </View>
     </View>
   );
@@ -36,6 +45,10 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     fontWeight: "500",
+  },
+
+  userContainer: {
+    marginVertical: 40,
   },
 });
 
