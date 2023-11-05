7d9501460e86acdcb62cca1b64f7f975f518e9cb
Justice Nyaga <justiceevansmwema@gmail.com>
2023-11-05 20:59:03 +0300

    Create a Card component

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import React from "react";
+import { View } from "react-native";
+
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
-import AppButton from "./app/components/AppButton";
+import Card from "./app/components/Card";
 
 const App = () => {
-  return <WelcomeScreen />;
+  return (
+    <View
+      style={{
+        backgroundColor: "#f8f4f4",
+        padding: 20,
+        paddingTop: 100,
+      }}
+    >
+      <Card
+        title="Red jacket for sale!"
+        subTitle="$100"
+        image={require("./app/assets/jacket.jpg")}
+      />
+    </View>
+  );
 };
 
 export default App;
