e02b94836681a92bd76fad486838073fb01a782a
Justice Nyaga <justiceevansmwema@gmail.com>
2023-11-05 21:25:35 +0300

    Add ListItem component

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Image, View, StyleSheet } from "react-native";
 
 import AppText from "../components/AppText";
+import ListItem from "../components/ListItem";
 
 import colors from "../config/colors";
 
@@ -13,6 +14,14 @@ const ListingDetailsScreen = () => {
       <View style={styles.detailsContainer}>
         <AppText style={styles.title}>Red jacket for sale!</AppText>
         <AppText style={styles.price}>$100</AppText>
+
+        <View style={styles.userContainer}>
+          <ListItem
+            image={require("../assets/justice.jpg")}
+            title="Justice Nyaga"
+            subTitle="5 Listings"
+          />
+        </View>
       </View>
     </View>
   );
@@ -35,6 +44,9 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     fontWeight: "500",
+  },
+  userContainer: {
+    marginVertical: 40,
   },
 });
 
