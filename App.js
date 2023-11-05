a6e0f956870880b9a0fc17c93804573ea0e2cdb5
Justice Nyaga <justiceevansmwema@gmail.com>
2023-11-05 18:57:29 +0300

    Encapsulating styles

--- App.js
+++ App.js
@@ -1,24 +1,12 @@
 import React from "react";
 import { View, Text } from "react-native";
 
+import AppText from "./components/AppText";
+
 export default function App() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text
-        style={{
-          fontSize: 26,
-          fontStyle: "italic",
-          fontWeight: "600",
-          color: "tomato",
-          textTransform: "capitalize",
-          textDecorationLine: "line-through",
-          textAlign: "center",
-          lineHeight: 30,
-        }}
-      >
-        I love react native! This is my first react native app! Here is some
-        more text.
-      </Text>
+      <AppText> I love react native! </AppText>
     </View>
   );
 }
