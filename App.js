733e33e350f663178f0fa314f6a34a7b2284d23a
Justice Nyaga <justiceevansmwema@gmail.com>
2023-11-05 18:08:40 +0300

    Borders

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello Justice!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: "dodgerblue",
+          borderWidth: 10,
+          borderColor: "royalblue",
+          // borderRadius: 10,
+          // borderTopWidth: 20,
+          // borderTopLeftRadius: 50,
+
+          // to create a cirlce, make sure that the border radius is atleast half the size of the view
+          borderRadius: 50,
+        }}
+      ></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
