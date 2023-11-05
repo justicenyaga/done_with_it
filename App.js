a076a6a27b40c5e42697caeecc044e90453ed943
Justice Nyaga <justiceevansmwema@gmail.com>
2023-11-05 19:48:10 +0300

    Create a custom AppButton

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
+import AppButton from "./app/components/AppButton";
+import { View } from "react-native";
 
 const App = () => {
-  return <ViewImageScreen />;
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <AppButton title="Login" onPress={() => console.log("Tapped")} />
+    </View>
+  );
 };
 
 export default App;
