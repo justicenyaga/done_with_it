a84c230e725a9afc56abab7471bbe4bbbd884f44
Justice Nyaga <justiceevansmwema@gmail.com>
2023-11-05 23:35:51 +0300

    Implement refreshing the messages list

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -23,6 +23,7 @@ const initialMessages = [
 
 const MessagesScreen = () => {
   const [messages, setMessages] = useState(initialMessages);
+  const [refreshing, setRefreshing] = useState(false);
 
   const handleDelete = (message) => {
     setMessages(messages.filter((m) => m.id !== message.id));
@@ -45,6 +46,17 @@ const MessagesScreen = () => {
           />
         )}
         ItemSeparatorComponent={ListItemSeparator}
+        refreshing={refreshing}
+        onRefresh={() =>
+          setMessages([
+            {
+              id: 2,
+              title: "T2",
+              description: "D2",
+              image: require("../assets/justice.jpg"),
+            },
+          ])
+        }
       />
     </Screen>
   );
