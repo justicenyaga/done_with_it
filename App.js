a1235c0a36f8113126a33e55b187964b9f0a2220
Justice Nyaga <justiceevansmwema@gmail.com>
2023-11-05 15:42:40 +0300

    Aligning items - justifyContent, alignItems, alignSelf

--- App.js
+++ App.js
@@ -1,35 +1,39 @@
-import {
-  StyleSheet,
-  SafeAreaView,
-  Platform,
-  StatusBar,
-  View,
-  Dimensions,
-} from "react-native";
-import { useDeviceOrientation } from "@react-native-community/hooks";
+import React from "react";
+import { View } from "react-native";
 
 export default function App() {
-  const orientation = useDeviceOrientation();
-
   return (
-    <SafeAreaView style={styles.container}>
+    <View
+      style={{
+        backgroundColor: "#fff",
+        flex: 1, // fill all the available space
+        flexDirection: "row", // define the main axis
+        justifyContent: "center", // align along the main (Primary) axis
+        alignItems: "center", // align along the secondary axis
+      }}
+    >
       <View
         style={{
-          width: "100%",
-          height: orientation === "portrait" ? "30%" : "100%",
+          width: 100,
+          height: 200,
           backgroundColor: "dodgerblue",
+          alignSelf: "flex-start", // aligning individual items along the secondary axis
         }}
       />
-    </SafeAreaView>
+      <View
+        style={{
+          width: 100,
+          height: 150,
+          backgroundColor: "gold",
+        }}
+      />
+      <View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: "tomato",
+        }}
+      />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-    // justifyContent: "center",
-    // alignItems: "center",
-  },
-});
