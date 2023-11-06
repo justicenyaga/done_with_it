e98ef7d86ef9a5b52efb67420f99a82451497ced
Justice Nyaga <justiceevansmwema@gmail.com>
2023-11-06 17:45:38 +0300

    Create login form boilerplate code

--- App.js
+++ App.js
@@ -1,30 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 
-import Screen from "./app/components/Screen";
-import AppTextInput from "./app/components/AppTextInput";
-import AppPicker from "./app/components/AppPicker";
-
-const categories = [
-  { value: 1, label: "Furnitures" },
-  { value: 2, label: "Clothing" },
-  { value: 3, label: "Cameras" },
-];
+import LoginScreen from "./app/screens/LoginScreen";
 
 const App = () => {
-  const [category, setCategory] = useState(categories[0]);
-
-  return (
-    <Screen>
-      <AppPicker
-        selectedItem={category}
-        onSelectItem={(item) => setCategory(item)}
-        items={categories}
-        icon="apps"
-        placeholder="Category"
-      />
-      <AppTextInput icon="email" placeholder="Email" />
-    </Screen>
-  );
+  return <LoginScreen />;
 };
 
 export default App;
