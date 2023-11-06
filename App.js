78c25b230438d10ad9e013800ce4a2bfcc944e79
Justice Nyaga <justiceevansmwema@gmail.com>
2023-11-06 08:00:41 +0300

    Implement Listings screen

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-import AccountScreen from "./app/screens/AccountScreen";
+import ListingsScreen from "./app/screens/ListingsScreen";
 
 const App = () => {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <AccountScreen />
+      <ListingsScreen />
     </GestureHandlerRootView>
   );
 };
