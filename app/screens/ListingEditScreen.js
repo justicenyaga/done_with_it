fafbe38082d93f2c118371b3089ab43641322970
Justice Nyaga <justiceevansmwema@gmail.com>
2023-12-15 20:31:43 +0300

    Implement a feature to get last known location on Listing Edit screen

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 import * as Yup from "yup";
+import * as Location from "expo-location";
 
 import {
   Form,
@@ -78,6 +79,22 @@ const categories = [
 ];
 
 const ListingEditScreen = () => {
+  const [location, setLocation] = useState();
+
+  const getLocation = async () => {
+    const { granted } = await Location.requestBackgroundPermissionsAsync();
+    if (!granted) return;
+
+    const {
+      coords: { longitude, latitude },
+    } = await Location.getLastKnownPositionAsync();
+    setLocation({ longitude, latitude });
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
   return (
     <Screen style={styles.container}>
       <Form
@@ -88,7 +105,7 @@ const ListingEditScreen = () => {
           category: null,
           images: [],
         }}
-        onSubmit={(values) => console.log(values)}
+        onSubmit={(values) => console.log(location)}
         validationSchema={validationSchema}
       >
         <FormImagePicker name="images" />
