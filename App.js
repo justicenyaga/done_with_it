db73d1061e7db7aaf5ff333a5d7a5e9187a7a3ba
Justice Nyaga <justiceevansmwema@gmail.com>
2023-12-17 13:38:54 +0300

    Navigating between different screens

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
 import React from "react";
-import { Text } from "react-native";
+import { Button, Text } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
 
 import Screen from "./app/components/Screen";
 
-const Tweets = () => (
+const Link = () => {
+  const navigation = useNavigation();
+  return (
+    <Button title="Click" onPress={() => navigation.navigate("TweetDetails")} />
+  );
+};
+
+const Tweets = ({ navigation }) => (
+  // navigation prop works only on Screen elements... it's not passed down to children elements
+  // To navigate to different screen/routes, use the navigation hook in "@react-navigation/native" module
+
   <Screen>
     <Text>Tweets</Text>
+    {/* <Button
+      title="View Tweet"
+      onPress={() => navigation.navigate("TweetDetails")}
+    /> */}
+    <Link />
   </Screen>
 );
 const TweetDetails = () => (
