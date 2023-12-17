96ddc70458ca7cdd9d25ef7501832a83aac620fe
Justice Nyaga <justiceevansmwema@gmail.com>
2023-12-17 13:16:06 +0300

    Stack Navigator

--- App.js
+++ App.js
@@ -1,9 +1,36 @@
 import React from "react";
+import { Text } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
 
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import Screen from "./app/components/Screen";
 
-const App = () => {
-  return <ListingEditScreen />;
+const Tweets = () => (
+  <Screen>
+    <Text>Tweets</Text>
+  </Screen>
+);
+const TweetDetails = () => (
+  <Screen>
+    <Text>TweetDetails</Text>
+  </Screen>
+);
+
+const Stack = createNativeStackNavigator();
+const StackNavigator = () => (
+  <Stack.Navigator initialRouteName="Tweets">
+    {/* if the initialRouteName is not provided.. renders the first screen as the initialRoute */}
+    <Stack.Screen name="TweetDetails" component={TweetDetails} />
+    <Stack.Screen name="Tweets" component={Tweets} />
+  </Stack.Navigator>
+);
+
+const App = (props) => {
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
 };
 
 export default App;
