e8a5df26ec8b970a2a4223fd460155ff2ed57921
Justice Nyaga <justiceevansmwema@gmail.com>
2023-12-18 21:02:26 +0300

    Handling errors

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { FlatList, StyleSheet, View } from "react-native";
 
+import Button from "../components/Button";
 import Card from "../components/Card";
 import Screen from "../components/Screen";
+import Text from "../components/Text";
 
 import colors from "../config/colors";
 import listingsApi from "../api/listings";
@@ -10,6 +12,7 @@ import routes from "../navigation/routes";
 
 const ListingsScreen = ({ navigation }) => {
   const [listings, setListings] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     loadListings();
@@ -17,12 +20,23 @@ const ListingsScreen = ({ navigation }) => {
 
   const loadListings = async () => {
     const response = await listingsApi.getListings();
+    if (!response.ok) return setError(true);
+
+    setError(false);
     setListings(response.data);
   };
 
   return (
     <Screen style={styles.screen}>
       <View style={styles.container}>
+        {error && (
+          <>
+            <Text style={{ textAlign: "center" }}>
+              Couldn't retrieve the listings
+            </Text>
+            <Button title="Retry" onPress={loadListings} />
+          </>
+        )}
         <FlatList
           data={listings}
           keyExtractor={(listing) => listing.id.toString()}
