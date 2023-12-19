6d7f8fc34bbe1d80936cfb9ce8ec827b6793c9cd
Justice Nyaga <justiceevansmwema@gmail.com>
2023-12-19 14:14:19 +0300

    Add an api function to create a new listing

--- app/api/listings.js
+++ app/api/listings.js
@@ -2,8 +2,33 @@ import client from "./client";
 
 const endpoint = "/listings";
 
+const addListing = (listing) => {
+  const data = new FormData();
+
+  data.append("title", listing.title);
+  data.append("price", listing.price);
+  data.append("categoryId", listing.category.value);
+  data.append("description", listing.description);
+
+  listing.images.forEach((image, index) =>
+    data.append("images", {
+      name: "image" + index,
+      type: "image/jpeg",
+      uri: image,
+    })
+  );
+
+  if (listing.location)
+    data.append("location", JSON.stringify(listing.location));
+
+  return client.post(endpoint, data, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+};
+
 const getListings = () => client.get(endpoint);
 
 export default {
+  addListing,
   getListings,
 };
