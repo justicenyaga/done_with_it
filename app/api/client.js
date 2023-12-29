b5b192d789f3d909fc95ad244f5f27cc4f9871e0
Justice Nyaga <justiceevansmwema@gmail.com>
2023-12-29 15:55:55 +0300

    Calling protected APIs

--- app/api/client.js
+++ app/api/client.js
@@ -1,12 +1,19 @@
 import { create } from "apisauce";
 import cache from "../util/cache";
+import authStorage from "../auth/storage";
 
 const apiClient = create({
   baseURL: "http://192.168.246.228:9000/api",
 });
 
-const get = apiClient.get;
+apiClient.addAsyncRequestTransform(async (request) => {
+  const authToken = await authStorage.getToken();
+  if (!authToken) return;
 
+  request.headers["x-auth-token"] = authToken;
+});
+
+const get = apiClient.get;
 apiClient.get = async (url, params, axiosConfig) => {
   const response = await get(url, params, axiosConfig);
 
