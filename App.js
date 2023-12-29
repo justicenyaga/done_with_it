560cfe1b19e24488b667ea164e7d7f5412a86a2e
Justice Nyaga <justiceevansmwema@gmail.com>
2023-12-29 15:01:52 +0300

    Controlling the splash screen

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { jwtDecode } from "jwt-decode";
 import "core-js/stable/atob";
+import * as SplashScreen from "expo-splash-screen";
 
 import AppNavigator from "./app/navigation/AppNavigator";
 import AuthNavigator from "./app/navigation/AuthNavigator";
@@ -13,28 +14,48 @@ import OfflineNotice from "./app/components/OfflineNotice";
 import AuthContext from "./app/auth/context";
 import authStorage from "./app/auth/storage";
 
+// Keep the splash screen visible while we fetch the resources
+SplashScreen.preventAutoHideAsync();
+
 const App = () => {
   const [user, setUser] = useState();
+  const [isReady, setIsReady] = useState(false);
 
   const restoreToken = async () => {
     const token = await authStorage.getToken();
-    if (!token) return;
+
+    if (!token) return setIsReady(true);
+
     setUser(jwtDecode(token));
+    setIsReady(true);
   };
 
   useEffect(() => {
     restoreToken();
   }, []);
 
+  const onLayoutRootView = useCallback(async () => {
+    if (isReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [isReady]);
+
+  if (!isReady) {
+    return null;
+  }
+
   return (
-    <AuthContext.Provider value={{ user, setUser }}>
-      <GestureHandlerRootView style={styles.container}>
+    <GestureHandlerRootView
+      style={styles.container}
+      onLayout={onLayoutRootView}
+    >
+      <AuthContext.Provider value={{ user, setUser }}>
         <OfflineNotice />
         <NavigationContainer theme={navigationTheme}>
           {user ? <AppNavigator /> : <AuthNavigator />}
         </NavigationContainer>
-      </GestureHandlerRootView>
-    </AuthContext.Provider>
+      </AuthContext.Provider>
+    </GestureHandlerRootView>
   );
 };
 
